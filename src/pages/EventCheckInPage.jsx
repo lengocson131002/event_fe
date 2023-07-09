3dfe96f7874446a37308b3f6d297676379df8078
minhduc <minhductxqt@gmail.com>
2023-07-09 20:02:37 +0700

    fix alert

--- src/pages/EventCheckInPage.jsx
+++ src/pages/EventCheckInPage.jsx
@@ -69,12 +69,14 @@ const EventCheckInPage = () => {
       {
         <Spin spinning={loading}>
           <div class='flex flex-col justify-between px-4 mx-auto max-w-screen-xl'>
-            <Alert
-              message='Please login before check-in'
-              type='warning'
-              showIcon
-              style={{ marginTop: '1rem' }}
-            />
+            {!auth && (
+              <Alert
+                message='Please login before check-in'
+                type='warning'
+                showIcon
+                style={{ marginTop: '1rem' }}
+              />
+            )}
             {/* <article class='mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue'> */}
             <main class='w-full pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white'>
               {!loading ? (
